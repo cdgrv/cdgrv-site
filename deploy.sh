41f64469180302c7aa0c1596bf01d14628021f83
JP Aumasson <jeanphilippe.aumasson@gmail.com>
2022-02-20 11:10:46 +0100

    cleanup

--- deploy.sh
+++ deploy.sh
@@ -3,23 +3,8 @@
 # If a command fails then the deploy stops
 set -e
 
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+hugo
 
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+cp -r public/* ../cdgrv.github.io
 
-# Go To Public folder
-cd public
-
-# Add changes to git.
-git add .
-
-# Commit changes.
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
-fi
-git commit -m "$msg"
-
-# Push source and build repos.
-git push 
+cd ../cdgrv.github.io && git commit -am new && git push
